4e3a5a97751b076f4062a46cbe33318e544b8f79
OcheOps <ochecodes@gmail.com>
2024-05-19 19:47:44 +0100

    fix??

--- app/handle.go
+++ app/handle.go
@@ -11,28 +11,28 @@ import (
 )
 
 func handleConnection(c net.Conn) {
-    defer c.Close()
-    reader := bufio.NewReader(c)
-    for {
-        line, err := reader.ReadString('\n')
-        if err != nil {
-            if err != io.EOF {
-                fmt.Println("Read error:", err)
-            }
-            break
-        }
-
-        parts := strings.Split(strings.TrimSpace(line), "\r\n")
-        if len(parts) < 4 {
-            continue
-        }
-
-        command := parts[1]
-        if command == "$4\r\nPING" {
-            c.Write([]byte("+PONG\r\n"))
-        } else if command == "$4\r\nECHO" {
-            echoText := parts[3]
-            c.Write([]byte("$" + strconv.Itoa(len(echoText)) + "\r\n" + echoText + "\r\n"))
-        }
-    }
+	defer c.Close()
+	reader := bufio.NewReader(c)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Read error:", err)
+			}
+			break
+		}
+	
+		parts := strings.Split(strings.TrimSpace(line), "\r\n")
+		if len(parts) < 4 {
+			continue
+		}
+	
+		command := parts[1]
+		if command == "$4\r\nPING" {
+			c.Write([]byte("+PONG\r\n"))
+		} else if command == "$4\r\nECHO" {
+			echoText := parts[3]
+			c.Write([]byte("$" + strconv.Itoa(len(echoText)) + "\r\n" + echoText + "\r\n"))
+		}
+	}
 }
