00666016a1798e49e493c6a7d57512e61cbdf5ac
OcheOps <ochecodes@gmail.com>
2024-05-19 19:18:09 +0100

    3rd stage

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -13,16 +14,33 @@ func main() {
 	//Uncomment this block to pass the first stage
 	
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	conn.Write([]byte("+PONG\r\n"))
+if err != nil {
+    fmt.Println("Failed to bind to port 6379")
+    os.Exit(1)
+}
+
+for {
+    conn, err := l.Accept()
+    if err != nil {
+        fmt.Println("Error accepting connection: ", err.Error())
+        os.Exit(1)
+    }
+
+    go func(c net.Conn) {
+        defer c.Close()
+        for {
+            buffer := make([]byte, 1024)
+            _, err := c.Read(buffer)
+            if err != nil {
+                if err != io.EOF {
+                    fmt.Println("Read error:", err)
+                }
+                break
+            }
+            c.Write([]byte("+PONG\r\n"))
+        }
+    }(conn)
+}
 }
 
 
